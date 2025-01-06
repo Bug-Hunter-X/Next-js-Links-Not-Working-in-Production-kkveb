12669d832c056a79a86757d1f77efa90c429c7a9
BugHunter-X <askzenai@gmail.com>
2025-01-06 06:58:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,22 @@
+```javascript
+import Link from 'next/link';
+
+function MyComponent() {
+  return (
+    <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>    </div>
+  );
+}
+
+export default MyComponent;
+
+// next.config.js
+module.exports = {
+  basePath: '/my-app',
+};
+```
